049782de2840427b3bac3e11c1cc7fa7508af2a9
Mhmtshn6 <mehmet68064@gmail.com>
2024-02-24 23:19:42 +0300

    Basit Inheritance Example
    Simple example of Inheritance , one of the basic principles of oop

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TeachersInformation teacher1 = new TeachersInformation();
+            teacher1.Name = "Admin";
+            teacher1.Age = 41;
+
+            StudenInformation student1 = new StudenInformation();
+            student1.Name = "Student1";
+            student1.Age = 13;
+            student1.StudentNumber = 103;
+        }
+    }
+    class Information
+    {
+        public string Name;
+        public int Age;
+    }
+    class TeachersInformation : Information { }
+
+    class StudenInformation : Information
+    {
+        public int StudentNumber;
+    }
+}
